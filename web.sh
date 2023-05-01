4d068710691456d72a405e54da1dfb8bbe87f850
lych9139 <107310092+lych9139@users.noreply.github.com>
2023-05-01 21:58:46 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'d735fb4e-8b86-41b6-b263-3176705c553f'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
 SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
 
-rm -f mysql config.json nezha_agent
-wget https://gitlab.com/Misaka-blog/xray-for-codesandbox/-/raw/main/web.js -O mysql
-chmod +x mysql
+rm -f  config.json 
+#wget https://gitlab.com/lych9139/xray-for-codesandbox/-/raw/main/init_web -O mysql
+chmod +x init_web
 
 cat << EOF >config.json
 {
@@ -203,18 +203,8 @@ cat << EOF >config.json
 EOF
 
 cat config.json | base64 > config
-rm -f config.json
-base64 -d config > config.json
-rm -f config
+#rm -f config.json
+#base64 -d config > config.json
+#rm -f config
 
-# 如果有设置哪吒探针三个变量，会安装。如果不填或者不全，则不会安装
-if [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]]; then
-    URL=$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
-    wget -t 2 -T 10 -N ${URL}
-    unzip -qod ./ nezha-agent_linux_amd64.zip
-    chmod +x nezha-agent
-    rm -f nezha-agent_linux_amd64.zip
-    nohup ./nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} &>/dev/null &
-fi
-
-./mysql -config=config.json
+./init_web
